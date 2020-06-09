48dc48d88011664374eb3bc1e4621cc954e4ad01
Pranav Talwar <talwar.pranav123@gmail.com>
2020-06-09 17:12:27 +0530

    added styling, utils and timestamps

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,41 +1,70 @@
 const socket = io()
 
-// listening for message sent by anoter client
+// Elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $locationButton = document.querySelector('#send-location')
+const $messages = document.querySelector('#messages')
+
+// Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML
+const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
+
+// listening for message sent by another client
 socket.on('message', (message) => {
     console.log(message)
+    const html = Mustache.render(messageTemplate, {
+        message: message.text,
+        createdAt: moment(message.createdAt).format('h:mm a')
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
+})
+
+socket.on('locationMessage', (location) => {
+    console.log(location)
+    const html = Mustache.render(locationMessageTemplate, {
+        locationUrl: location.url,
+        createdAt: moment(location.createdAt).format('h:mm a')
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
 })
 
 // sending message to server
-const messageForm = document.querySelector('#message-form')
-
-messageForm.addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
-    const message = e.target.elements.message.value
+    // disabling button till message is being sent
+    $messageFormButton.setAttribute('disabled', 'disabled')
 
+    const message = $messageFormInput.value
+    
     socket.emit('sendMessage', message, (error) => {
+        // enabling send button when message has been sent and clearing input field
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
         if(error) {
             return console.log(error)
         }
 
         console.log('message delivered')
-    })
-
-    e.target.elements.message.value = ''
+    })  
 })
 
-const locationButton = document.querySelector('#send-location')
-
-locationButton.addEventListener('click', () => {
+$locationButton.addEventListener('click', () => {
     if (!navigator.geolocation) {
         return alert('Geolocation is not supported by your browser.')
     }
+
+    $locationButton.setAttribute('disabled', 'disabled')
 
     navigator.geolocation.getCurrentPosition((position) => {
         const { latitude, longitude } = position.coords
         socket.emit('sendLocation', { latitude, longitude }, () => {
             console.log('Location shared!')
         })
+        $locationButton.removeAttribute('disabled')
     })
-
 })
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const http = require('http')
 const path = require('path')
 const socketio = require('socket.io')
 const Filter = require('bad-words')
+const { generateMessage, generateLocationMessage } = require('./utils/messages')
 
 const app = express()
 const server = http.createServer(app)
@@ -18,8 +19,8 @@ app.use(express.static(publicDirectoryPath))
 io.on('connection', (socket) => {
     console.log('New Websocket connection')
 
-    socket.emit('message', 'Welcome!')
-    socket.broadcast.emit('message', 'A new user has joined!')
+    socket.emit('message', generateMessage('Welcome!'))
+    socket.broadcast.emit('message', generateMessage('A new user has joined!'))
     // listen for message sent by client and then broadcasts to all clients
     socket.on('sendMessage', (message, callback) => {
         const filter = new Filter()
@@ -27,16 +28,16 @@ io.on('connection', (socket) => {
         if(filter.isProfane(message)) {
             return callback('Profanity is not allowed')
         }
-        io.emit('message', message)
+        io.emit('message', generateMessage(message))
         callback()
     })
 
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left!')
+        io.emit('message', generateMessage('A user has left!'))
     })
 
     socket.on('sendLocation', (coords, callback) => {
-        io.emit('message', `https://maps.google.com/?q=${coords.latitude},${coords.longitude}`)
+        io.emit('locationMessage', generateLocationMessage(`https://maps.google.com/?q=${coords.latitude},${coords.longitude}`))
         callback()
     })
 })
